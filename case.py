529cfc00a7f8f8cf26f8834b4d37e80184856541
Felix♨ <felipedasilvapereira@outlook.com>
2025-02-18 00:23:13 -0300

    feat: Implementing connection in DB and adding repository

---
+++ case.py
@@ -0,0 +1,11 @@
+# Ilustração do que faremos na nossa classe de ilustração
+class MinhaClasse:
+    def __enter__(self):
+        print("Entrei!!!")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Sai!!!")
+
+
+with MinhaClasse() as mc: 
+    print("Estou aqui dentro!")
